6693ef2c24b0d4577b277424dcef0845367a8662
rezaputraprimayanto <152097319+rezaputraprimayanto@users.noreply.github.com>
2023-12-14 10:41:49 +0700

    Create DML TabelTransaksi.sql

---
+++ DML TabelTransaksi.sql
@@ -0,0 +1,48 @@
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(1,
+"Lunas", "2022-03-03", 20000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(2,
+"Transfer", "2022-03-11", 20000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(4,
+"Transfer", "2022-03-25", 20000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(3,
+"Lunas", "2022-04-11", 20000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(4,
+"Lunas", "2022-04-07", 10000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(1,
+"Transfer", "2022-04-09", 20000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(3,
+"Belum Lunas", "2022-03-20", 15000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(4,
+"Lunas", "2022-04-16", 30000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(2,
+"Belum Lunas", "2022-04-19", 15000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(1,
+"Lunas", "2022-04-22", 30000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(2,
+"Lunas", "2022-03-03", 40000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(3,
+"Belum Lunas", "2022-03-03", 35000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(3,
+"Lunas", "2022-03-11", 50000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(1,
+"Lunas", "2022-03-11", 15000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(4,
+"Transfer", "2022-03-11", 5000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(4,
+"Transfer", "2022-03-20", 10000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(3,
+"Transfer", "2022-04-07", 10000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(1,
+"Lunas", "2022-04-07", 20000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(2,
+"Transfer", "2022-04-07", 30000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(4,
+"Transfer", "2022-04-09", 15000, "Diambil");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(2,
+"Belum Lunas", "2022-04-15", 40000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(3,
+"Belum Lunas", "2022-04-15", 35000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(1,
+"Transfer", "2022-04-11", 5000, "Tertahan");
+INSERT INTO transaksi(idPelanggan, status, tanggal, harga, kondisi) VALUES(4,
+"Lunas", "2022-04-13", 25000, "Diambil");
