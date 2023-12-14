400dadae4dd08f57a53517bed40b5d73d52b791f
rezaputraprimayanto <152097319+rezaputraprimayanto@users.noreply.github.com>
2023-12-14 10:40:21 +0700

    Create DML TabelPelanggan.sql

---
+++ DML TabelPelanggan.sql
@@ -0,0 +1,4 @@
+INSERT INTO pelanggan(idPelanggan, namaPelanggan) VALUES(1, "faiz");
+INSERT INTO pelanggan(idPelanggan, namaPelanggan) VALUES(2, "gifar");
+INSERT INTO pelanggan(idPelanggan, namaPelanggan) VALUES(3, "alif");
+INSERT INTO pelanggan(idPelanggan, namaPelanggan) VALUES(4, "rafi");
